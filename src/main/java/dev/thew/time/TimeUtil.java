1cb9041e298864ae3c7b54115bb1090be0289e14
QwThew <animesenpai.kimi@gmail.com>
2025-01-12 05:41:04 +0500

    1.0.8

--- src/main/java/dev/thew/time/TimeUtil.java
+++ src/main/java/dev/thew/time/TimeUtil.java
@@ -2,16 +2,38 @@ package dev.thew.time;
 
 public class TimeUtil {
 
-    public static String getTimeEnding(int number, String form1, String form2, String form5) {
-        number = Math.abs(number);
-        int n100 = number % 100;
-        int n10 = number % 10;
-
-        if (n100 >= 11 && n100 <= 14) return form5;
-        if (n10 == 1) return form1;
-        if (n10 >= 2 && n10 <= 4) return form2;
-
+    private static String getWordForm(long number, String form1, String form2, String form5) {
+        number = Math.abs(number) % 100;
+        long n = number % 10;
+        if (number > 10 && number < 20) return form5;
+        if (n > 1 && n < 5) return form2;
+        if (n == 1) return form1;
         return form5;
+    }
+
+    public static String getTimeEnding(long seconds) {
+        long hours = seconds / 3600;
+        long minutes = (seconds % 3600) / 60;
+        long secs = seconds % 60;
+
+        StringBuilder result = new StringBuilder();
+
+        if (hours > 0) result
+                .append(hours)
+                .append(" ")
+                .append(getWordForm(hours, "час", "часа", "часов"))
+                .append(" ");
+        if (minutes > 0) result
+                .append(minutes)
+                .append(" ")
+                .append(getWordForm(minutes, "минута", "минуты", "минут"))
+                .append(" ");
+        if (secs > 0 || result.isEmpty()) result
+                .append(secs)
+                .append(" ")
+                .append(getWordForm(secs, "секунда", "секунды", "секунд"));
+
+        return result.toString().trim();
     }
 
 }
